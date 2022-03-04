775eda801cf6b0d93421e243348e4090a176a46a
besika94 <besika94@gmail.com>
2022-03-04 15:39:43 +0400

    nova slider done

--- nova/js/main.js
+++ nova/js/main.js
@@ -2,10 +2,11 @@ $(function () {
   //small image displays into prev img
   $(".small-img img").click(function (e) {
     let src = $(this).attr("src");
+    let dataIndex = $(this).attr("data-index");
     $(this).addClass("active").siblings().removeClass("active");
     $(".prev-img img").attr("src", src);
+    $(".prev-img img").attr("data-index", $(this).attr("data-index"));
   });
-
   // prev img opens in modal
   $(".prev-img img").click(function (e) {
     e.preventDefault();
@@ -14,14 +15,62 @@ $(function () {
     $(".modal").css({
       display: "block",
     });
+    //take data from clicked img and give to modal image
+    let activeData = $(this).attr("data-index");
+    $.each($(".modal-images img"), function (i) {
+      if ($(this).attr("data-index") == activeData) {
+        $(this).addClass("active").siblings().removeClass("active");
+      }
+    });
+  });
+  // next button function
+  $(".next").click(function (e) {
+    e.preventDefault();
+    //check if it has sibling
+    if ($(".modal-images img.active").next().length > 0) {
+      $(".modal-images img.active")
+        .removeClass("active")
+        .next()
+        .addClass("active");
+      let src = $(".modal-images img.active").attr("src");
+      $(".modal-prev").attr("src", src);
+    }
+  });
+  // prev button function
+  $(".prev").click(function (e) {
+    e.preventDefault();
+    //check if it has sibling
+    if ($(".modal-images img.active").prev().length > 0) {
+      $(".modal-images img.active")
+        .removeClass("active")
+        .prev()
+        .addClass("active");
+      let src = $(".modal-images img.active").attr("src");
+      $(".modal-prev").attr("src", src);
+    }
+  });
+  // after modal opens change pictures from bottom
+  $(".modal-images img").click(function (e) {
+    e.preventDefault();
+    let Src = $(this).attr("src");
+    $(".modal-prev").attr("src", Src);
+    $(this).addClass("active").siblings().removeClass("active");
   });
 
   // when clicked outside of modal , modal disapears
-  $(document).click(function (e) {
+  $(".modal").click(function (e) {
     e.preventDefault();
     if (e.target.classList == "modal") {
       $(".modal").css({
         display: "none",
+      });
+      let src = $(".modal-images img.active").attr("src");
+      let activeData = $(".modal-images img.active").attr("data-index");
+      $(".prev-img img").attr("src", src);
+      $.each($(".small-img img"), function () {
+        if ($(this).attr("data-index") == activeData) {
+          $(this).addClass("active").siblings().removeClass("active");
+        }
       });
     }
   });
