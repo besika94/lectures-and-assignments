22dc5cfd6ecafe5c602533dfe58e72e4bf4847b6
besika94 <96308647+besika94@users.noreply.github.com>
2022-09-28 12:15:04 +0100

    Delete dashboard directory

--- dashboard/main.js
+++
@@ -1,9 +0,0 @@
-const show = document.getElementById("humb");
-const leftPanel = document.querySelector(".left-panel");
-const full = document.getElementById("full");
-const side = document.querySelector(".sidebar");
-show.addEventListener("click", function () {
-  leftPanel.classList.toggle("active");
-  full.classList.toggle("goFull");
-  side.classList.toggle("hide");
-});
